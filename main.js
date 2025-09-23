52f9999cff0628f35997f30631911d0cf07fde09
Sam-MARTis <samanthmartis@gmail.com>
2025-09-23 15:41:26 +0530

    Added normal shock class

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+class NormalShock {
+    static DownStreamMachNumber(M1, gamma) {
+        const t1 = (gamma - 1);
+        const m1sq = M1 * M1;
+        const m2sq = (t1 * m1sq + 2) / (2 * gamma * m1sq - t1);
+        return Math.sqrt(m2sq);
+    }
+    static P2_by_P1(M1, gamma) {
+        const m1sq = M1 * M1;
+        return (2 * gamma * m1sq - (gamma - 1)) / (gamma + 1);
+    }
+    static RHO2_by_RHO1(M1, gamma) {
+        const m1sq = M1 * M1;
+        return ((gamma + 1) * m1sq) / ((gamma - 1) * m1sq + 2);
+    }
+    static T2_by_T1(M1, gamma) {
+        const p2byb1 = NormalShock.P2_by_P1(M1, gamma);
+        const rho2byrho1 = NormalShock.RHO2_by_RHO1(M1, gamma);
+        return p2byb1 / rho2byrho1;
+    }
+    static A2_by_A1(M1, gamma) {
+        return Math.sqrt(NormalShock.T2_by_T1(M1, gamma));
+    }
+}
--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
 
-const canvas = document.getElementById("projectCanvas") as HTMLCanvasElement;
-canvas.width = window.innerWidth*devicePixelRatio;
-canvas.height = window.innerHeight*devicePixelRatio;
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-if(!ctx){
-    throw Error("Context unable to be found");
+class NormalShock{
+  static DownStreamMachNumber(M1:number, gamma:number):number {
+    const t1 = (gamma - 1);
+    const m1sq = M1*M1;
+    const m2sq = (t1*m1sq + 2)/(2*gamma*m1sq - t1);
+    return Math.sqrt(m2sq);
+  }
+  static P2_by_P1(M1:number, gamma:number):number {
+    const m1sq = M1*M1;
+    return (2*gamma*m1sq - (gamma - 1))/(gamma + 1);
+  }
+  static RHO2_by_RHO1(M1:number, gamma:number):number {
+    const m1sq = M1*M1;
+    return ((gamma + 1)*m1sq)/((gamma - 1)*m1sq + 2);
+  }
+  static T2_by_T1(M1:number, gamma:number):number {
+    const p2byb1 = NormalShock.P2_by_P1(M1, gamma);
+    const rho2byrho1 = NormalShock.RHO2_by_RHO1(M1, gamma);
+    return p2byb1/rho2byrho1;
+  }
+  static A2_by_A1(M1:number, gamma: number):number {
+    return Math.sqrt(NormalShock.T2_by_T1(M1, gamma));
+  }
+
+
 }
